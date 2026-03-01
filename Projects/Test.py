85b7c3a828e1aa8ffe5b1e187587d95705ec8ed4
ARGUMENTSNOWUGHHH <nah.id.win.286h@gmail.com>
2026-03-01 03:11:56 -0600

    Refactor script to use curses for terminal graphics

--- Projects/Test.py
+++ Projects/Test.py
@@ -1,15 +1,66 @@
-import sys
-import time
+import curses
 import random
+import time
 
-def killmyself(text):
-    for char in text:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(random.uniform(0.05, 0.2) if char.isalnum() else random.uniform(0.2, 0.5))
-        if random.random() < 0.1 and char.isalnum():
-            sys.stdout.write(random.choice(["~nya", " >///<", " hehe~"]))
-            sys.stdout.flush()
-            time.sleep(random.uniform(0.2, 0.5))
+logo = r"""
+ ╭━━━━╮
+ ┃╭╮╭╮┃
+ ╰╯┃┃┣┻━┳━┳━━╮
+ ╱╱┃┃┃┃━┫╭┫╭╮┃
+ ╱╱┃┃┃┃━┫┃┃╭╮┃
+ ╱╱╰╯╰━━┻╯╰╯╰╯"""
 
-killmyself("Ay sem- sem- Sempai! Go to the other project~ >///<")
+def kys(stdscr):
+    curses.curs_set(0)  
+    stdscr.nodelay(True)
+    stdscr.timeout(50)
+
+    sh, sw = stdscr.getmaxyx()
+    columns = [0] * sw
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()"
+
+    start_time = time.time()
+    Retard = False
+    logo_lines = logo.splitlines()
+    logo_h = len(logo_lines)
+    logo_w = max(len(line) for line in logo_lines)
+    logo_y = sh // 2 - logo_h // 2
+    logo_x = sw // 2 - logo_w // 2
+
+    while True:
+        for i in range(sw):
+            if random.random() > 0.9:
+                columns[i] = 0
+            char = random.choice(chars)
+            stdscr.addstr(columns[i], i, char, curses.color_pair(1))
+            columns[i] = (columns[i] + 1) % sh
+
+        if not Retard and time.time() - start_time >= 2:
+            for idx, line in enumerate(logo_lines):
+                stdscr.addstr(logo_y + idx, logo_x, line, curses.color_pair(2))
+            Retard = True
+
+        
+        if Retard:
+            for idx, line in enumerate(logo_lines):
+                stdscr.addstr(logo_y + idx, logo_x, line, curses.color_pair(2))
+        
+            exit_msg = "Press Y to exit"
+            stdscr.addstr(logo_y + logo_h + 1, sw // 2 - len(exit_msg) // 2, exit_msg, curses.color_pair(2))
+
+        stdscr.refresh()
+
+
+        try:
+            key = stdscr.getch()
+            if key in [ord('Y'), ord('y')]:
+                break
+        except:
+            pass
+
+if __name__ == "__main__":
+    curses.initscr()
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)  
+    curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)   
+    curses.wrapper(kys)
